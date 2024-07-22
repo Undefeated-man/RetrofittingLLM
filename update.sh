e2b9dd82dbe9d3a8d56f8f28ec520d4a2ab5f7c1
user name <s2484588-epcc@eidf018-msc-project2.vms.os.eidf.epcc.ed.ac.uk>
2024-07-22 10:23:22 +0000

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa_ret
+
 git pull git@github.com:Undefeated-man/RetrofittingLLM.git
 git add -A .
 git commit -m "update"
-git push --force
+git push main --force
